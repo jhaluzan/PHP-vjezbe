8ad18f861db3a1ebe9eb94e9bc3b5da2fe48c0d9
jhaluzan <106825854+jhaluzan@users.noreply.github.com>
2024-10-11 00:08:30 +0200

    Add files via upload

---
+++ vjezba1/zad1.php
@@ -0,0 +1,21 @@
+<?php
+
+print '
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>Da Vincijev kod</title>
+</head>
+<body>
+    <h1>Da Vincijev kod</h1>
+    <p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.</p>
+    <a href="https://hr.wikipedia.org/Da_Vincijev_kod" target="_blank">POVEZNICA</a>
+</body>
+</html>
+<!-- zad1 -->'
+
+?>
