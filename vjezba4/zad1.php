8a6efef52ebdc4c9b12994aff34c8845059bc846
jhaluzan <106825854+jhaluzan@users.noreply.github.com>
2024-10-11 19:14:40 +0200

    Add files via upload

---
+++ vjezba4/zad1.php
@@ -0,0 +1,17 @@
+<?php
+print '
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<form method="POST" action="zad1_izracun.php">
+    <label for="a">Vrijednost a:</label>
+    <input type="number" id="a" name="vrijednost_a"><br>
+    <label for="b">Vrijednost b:</label>
+    <input type="number" id="b" name="vrijednost_b"><br><br>
+    <button type="submit">Pošalji</button>
+</form>
+</html>'
+?>
