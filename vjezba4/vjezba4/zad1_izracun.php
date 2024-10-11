6aba4fa59c03221d519b22c0fe989f23289d439c
jhaluzan <106825854+jhaluzan@users.noreply.github.com>
2024-10-11 19:26:58 +0200

    Add files via upload

---
+++ vjezba4/vjezba4/zad1_izracun.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php
+        $a = $_POST['vrijednost_a'];
+        $b = $_POST['vrijednost_b'];
+        $c = (3 * $a - $b) / 2;
+
+        echo 'Vrijednost a: ' . $a . '<br>';
+        echo 'Vrijednost b: ' . $b . '<br>';
+        echo 'Vrijednost c = (3 * '. $a . '-' . $b . ') / 2 = ' . $c;
+    ?>
+</body>
+</html>
+
+
